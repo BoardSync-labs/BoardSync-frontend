67e9b73046e6801246410488f7867a7ad9c0fa16
Gauravi Kamat <gauravikamat77@gmail.com>
2025-12-13 00:32:21 +0530

    changes navbar for resgister orgnazation

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
               Features
             </Link>
             <Link to="/register-organization" className="text-sm text-muted-foreground hover:text-foreground transition-colors">
-              Pricing
+              register-organization
             </Link>
             <Link to="/" className="text-sm text-muted-foreground hover:text-foreground transition-colors">
               Documentation
